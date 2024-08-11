408394d1a387d87bde19f81bd29cbfbc5c7376db
Yasukaze-U <finalyasu@gmail.com>
2024-08-11 17:43:09 +0900

    ブログ投稿画面の作成

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <tytle>Blog</tytle>
+    </head>
+    <body>
+        <div class="top">
+            <h1>Blog title</h1>
+        </div>
+        <form action="/posts" method="POST">
+            <div>
+                <label for="title">タイトル</label>
+                <input type="text" id="title" name="title">
+            </div>
+            <div>
+                <label for="body">本文</label>
+                <input type="text" id="body" name="body">
+            </div>
+            <input type="submit" value="送信">
+        </form>
+        <a href="/">back</a>
+    </body>
+</html>
